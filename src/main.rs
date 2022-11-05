fc6f02cacb476c61afced62b1e9bcf989a267852
cannotilever <cannotilever@protonmail.com>
2022-11-05 17:31:01 -0400

    pain. Suffering, even. I can't get cookie storage with encryption working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use cookie::{Cookie, CookieJar, Key};
 
-fn main() {
-    println!("Hello, world!");
+use reqwest::{ClientBuilder, Client, cookie::Jar};
+use cookie::Cookie;
+use reqwest_cookie_store::*;
+#[tokio::main]
+async fn main() {
+    let jar = reqwest_cookie_store::CookieStore::default();
+    let jar = reqwest_cookie_store::CookieStoreMutex::new(jar);
+    let jar = std::sync::Arc::new(jar);
+    let client = reqwest::Client::builder().cookie_provider(std::sync::Arc::clone(&jar)).build().unwrap();
+    let test = client.post("http://127.0.0.1:8000/login").form(&[("ssn", "12345"),("password","1234")]).send().await.unwrap();
+    let bruh = client.post("http://127.0.0.1:8000/vote").form(&[("candidate","candidate1")]).send().await.unwrap();
+    let mut store = jar.lock().unwrap();
+
+  for c in store.iter_any() {
+    Cookie::build(c.name(), c.value());
+    println!("{:?}", c);
+  }
 }
